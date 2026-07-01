b08ed8a3b05ceba0c0327bea1b878b9f499d550a
Roberto Elola, Jorge Fabio <fabior1981@gmail.com>
2026-07-01 19:59:58 -0300

    777

--- js/config.js
+++ js/config.js
@@ -2,16 +2,21 @@
 // Modificar los valores según corresponda
 
 window.config = {
-        rubros: [
-            "Importador",
-            "Exportador",
-            "Despachante de Aduana",
-            "Comercio",
-            "Agente",
-            "Línea marítima",
-            "Aerolínea"
-        ],
+    rubros: [
+        "Importador",
+        "Exportador",
+        "Despachante de Aduana",
+        "Comercio",
+        "Agente",
+        "Línea marítima",
+        "Aerolínea"
+    ],
     googleCalendarAccount: "", // Cuenta de Gmail para futura integración
+    dashboardHome: {
+        maxClients: 15,
+        monthsOptions: [3, 6, 9, 12],
+        defaultMonths: 6
+    },
     // Otros parámetros de configuración pueden agregarse aquí
     sessionDuration: 8 * 60 * 60 * 1000 // Duración de sesión en milisegundos (8 horas)
 };
